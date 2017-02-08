7f4263f2c326c8abf18c9f2ce4747c4b0e07b1a1
g_grechin <g.grechin@poisk-it.ru>
2017-02-08 15:54:25 +0300

    Added VS solution with GeneralContext class

---
+++ src/General.Model/General.Model/Context/GeneralContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace PoiskIT.Okenit2.General.Context
+{
+    /// <summary>
+    /// Основной контекст.
+    /// Содержит все сущности. На его основе создается модель БД и миграции.
+    /// Не рекомендуется к прямому использованию.
+    /// </summary>
+    internal class GeneralContext : DbContext
+    {
+    }
+}
---
+++ src/General.Model/General.Model/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("General.Model")]
+[assembly: AssemblyDescription("Data model")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Poisk-IT")]
+[assembly: AssemblyProduct("General.Model")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f199892a-9139-482a-8682-f75e4e1b04f3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
