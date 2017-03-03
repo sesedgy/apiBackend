7ebd0eec9f19046bd3a34114a7ca16a18edcf1be
MLukanin <XSW@zaq1>
2017-03-03 16:52:39 +0300

    small fixes 2

---
+++ src/finalapp/CookieList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using finalapp.Models;
+
+namespace finalapp
+{
+    public class CookieList
+    {
+
+        private static CookieList _instance;
+        private Dictionary<Guid,Cookie> _dictionary;
+
+        private CookieList()
+        {
+            _dictionary = new Dictionary<Guid, Cookie>();
+        }
+
+        public static CookieList GetInstance()
+        {
+            if (_instance == null)
+                _instance = new CookieList();
+            return _instance;
+        }
+
+        public void AddCookie(User user)
+        {
+            var guid = new Guid();
+            var cookie = new Cookie() {Id = guid, UserId = user.Id,};
+            switch (user.Role)
+            {
+                case "IsAbiturient":
+                    cookie.IsAbiturient = true;
+                    break;
+                case "IsStudent":
+                    cookie.IsStudent = true;
+                    break;
+                case "IsStudentLeader":
+                    cookie.IsStudentLeader = true;
+                    break;
+                case "IsTeacher":
+                    cookie.IsTeacher = true;
+                    break;
+                case "IsWorker":
+                    cookie.IsWorker = true;
+                    break;
+                case "IsAdmin":
+                    cookie.IsAdmin = true;
+                    break;
+            }
+            _dictionary.Add(guid, cookie);
+        }
+
+        //TODO Возможна ошибка что отсутствующие роли будут вылетать, для этого надо будет исправить AddCookie
+        public bool CheckCookie(string guid, string[] rights)
+        {
+            if (_dictionary.ContainsKey(new Guid(guid)))
+            {
+                var cookie = _dictionary[new Guid(guid)];
+                for (int i = 0; i < rights.Length; i++)
+                {
+                    switch (rights[i])
+                    {
+                        case "IsAbiturient":
+                            if (cookie.IsAbiturient) { return true; }
+                            break;
+                        case "IsStudent":
+                            if (cookie.IsStudent) { return true; }
+                            break;
+                        case "IsStudentLeader":
+                            if (cookie.IsStudentLeader) { return true; }
+                            break;
+                        case "IsTeacher":
+                            if (cookie.IsTeacher) { return true; }
+                            break;
+                        case "IsWorker":
+                            if (cookie.IsWorker) { return true; }
+                            break;
+                        case "IsAdmin":
+                            if (cookie.IsAdmin) { return true; }
+                            break;
+                    } 
+                }
+            }
+            return false;
+        }
+        
+    }
+}
--- src/finalapp/Models/Cookie.cs
+++ src/finalapp/Models/Cookie.cs
@@ -9,5 +9,11 @@ namespace finalapp.Models
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
+        public bool IsAbiturient { get; set; }
+        public bool IsStudent { get; set; }
+        public bool IsStudentLeader { get; set; }
+        public bool IsTeacher { get; set; }
+        public bool IsWorker { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
