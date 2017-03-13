16bb7385f92b5d2f3322b95a5286cd45fcef4720
MLukanin <XSW@zaq1>
2017-03-13 15:34:24 +0300

    remove General.Model files

--- src/General.Model/General.Model/Context/GeneralContext.cs
+++ src/General.Model/General.Model/Context/GeneralContext.cs
@@ -1,5 +1,5 @@
 ﻿using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
+
 
 namespace PoiskIT.Okenit2.General.Context
 {
@@ -8,12 +8,7 @@ namespace PoiskIT.Okenit2.General.Context
     /// Содержит все сущности. На его основе создается модель БД и миграции.
     /// Не рекомендуется к прямому использованию.
     /// </summary>
-    internal sealed class GeneralContext : DbContext
+    internal class GeneralContext : DbContext
     {
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.HasDefaultSchema("public");
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        }
     }
 }
